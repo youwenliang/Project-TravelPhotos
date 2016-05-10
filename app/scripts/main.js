f0563192c30d9cf5eb43e5b2b1179927f62dbb86
Mark Liang <mark.liang@akqa.com>
2016-05-10 23:48:24 +0800

    Adding filtering

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,7 +9,7 @@ var tags = [];
 
 function getAlbumInfo(photoset_id) {
 	var request = new XMLHttpRequest();
-	request.open('GET', 'https://api.flickr.com/services/rest/?method=flickr.photosets.getInfo&api_key=7727dfc8d38ca43ff2d91ddc3d1e37f7&photoset_id='+photoset_id+'&user_id=129588168%40N02&format=json&nojsoncallback=1');
+	request.open('GET', 'https://api.flickr.com/services/rest/?method=flickr.photosets.getInfo&api_key=b5915b4e4a36d456caa767bdb9003cbc&photoset_id='+photoset_id+'&user_id=129588168%40N02&format=json&nojsoncallback=1');
 	request.setRequestHeader('Accept','application/json');
 
 	request.onreadystatechange = function () {
@@ -92,7 +92,7 @@ function getPhotoInfo(photo_id, photo_secret, k) {
 	    var data = jQuery.parseJSON(this.responseText);
 	    photos_info[k] = {
 	    	"photo_title":data.photo.title._content,
-			"photo_url":"https://farm"+data.photo.farm+".staticflickr.com/"+data.photo.server+"/"+data.photo.id+"_"+data.photo.secret+"_h.jpg",
+			"photo_url":"https://farm"+data.photo.farm+".staticflickr.com/"+data.photo.server+"/"+data.photo.id+"_"+data.photo.secret+"_n.jpg",
 			"photo_date":data.photo.dates.taken.split(" ")[0],
 			"photo_time":data.photo.dates.taken.split(" ")[1],
 			"photo_tags":data.photo.tags.tag,
@@ -107,6 +107,68 @@ function getPhotoInfo(photo_id, photo_secret, k) {
 	    	console.log(photos_info);
 	    	tags = unique(tags);
 	    	console.log(tags);
+
+	    	//Load Tags
+	    	for(var i = 0; i < tags.length; i++) {
+	    		$('.button-group').append('<button class="button" data-filter=".'+tags[i]+'">'+tags[i]+'</button>');
+	    	}
+	    	for(var i = 0; i < photos_info.length; i++) {
+	    		if(photos_info[i] != null) {
+		    		var phototags = "";
+		    		for(var j = 0; j < photos_info[i].photo_tags.length; j++) {
+				    	phototags += photos_info[i].photo_tags[j]._content;
+				    	phototags += " ";
+				    }
+		    		$('.grid').append('<div class="photo '+phototags+'" style="background-image: url('+photos_info[i].photo_url+')"></div>');
+		    	}
+		    	//Load Images
+		    	if(i + 1 == photos_info.length) {
+		    		var count = $('.photo').length;
+				    $('.photo').each(function(i){
+				        var $this = $(this);
+				        var img = new Image;
+				        img.src = $this.css('background-image').replace(/url\(\"|\"\)$/ig, "");
+				        $(img).one('load', function(){
+				            var bgImgWidth = img.width;
+				            var bgImgHeight = img.height;
+				            var newHeight = $this.width()*bgImgHeight/bgImgWidth;
+				            $this.css('height',newHeight);
+				            setTimeout(function(){
+				                $this.css('opacity', 1);
+				                // console.log(i+"==="+count);
+				                
+				                    // console.log("done!");
+				                    $('.grid').isotope({
+				                      itemSelector: '.photo',
+				                      masonry: {
+				                        // columnWidth: 212,
+				                        isFitWidth: true
+				                      }
+				                    });
+				                if(i == count-1) {
+				                    $('.grid').css('margin-left', ($(window).width()-$('.grid').width())/2);
+
+				                    $('.button-group').each( function( i, buttonGroup ) {
+									  var $buttonGroup = $( buttonGroup );
+									  $buttonGroup.on( 'click', 'button', function() {
+									    if($(this).hasClass('is-checked')) $(this).removeClass('is-checked');
+									    else $(this).addClass('is-checked');
+									    
+									    var filtervalue = "";
+									    $('.is-checked').each(function(i){ 
+									      filtervalue += $(this).attr('data-filter');
+									      
+									    });
+									    console.log(filtervalue);
+									    $('.grid').isotope({ filter: filtervalue });
+									  });
+									});
+				                }
+				            }, i*0);
+				        });
+				    });
+		    	}
+	    	}
 	    }
 	  }
 	};
@@ -128,5 +190,32 @@ function unique(list) {
 //Tokyo Trip - 72157649613805629
 var album_id = "72157667507455746";
 
-getAlbumInfo(album_id);
-getAlbumPhotos(album_id);
+$(document).ready(function(){
+	getAlbumInfo(album_id);
+	getAlbumPhotos(album_id);
+
+	$('#close').click(function(){
+	    $('.button-group').slideToggle('slow', function(){
+	    	$('#filtering span').css('display','block');
+	    	setTimeout(function(){
+	    		$('#filtering span').css('opacity','1');
+	    	}, 10);
+	    });
+	});
+	$('#filtering span').click(function(){
+	    $('.button-group').slideToggle('slow', function(){
+	    	$('#filtering span').css('opacity',0);
+	    	setTimeout(function(){
+	    		$('#filtering span').css('display','none');
+	    	}, 250);
+	    });
+	});
+})
+
+$( window ).resize(function() {
+    $('.grid').css('margin-left', ($(window).width()-$('.grid').width())/2);
+});
+
+$('.grid').on( 'layoutComplete', function( event, filteredItems ) {
+    $('.grid').css('margin-left', ($(window).width()-$('.grid').width())/2);
+});
